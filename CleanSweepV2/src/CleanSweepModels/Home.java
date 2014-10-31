fa5d6ddf1730c2a8ffdf95862493acfe69804370
unknown <chris.c.lacy@gmail.com>
2014-10-31 14:28:37 -0500

    Added ability to take path to XML as parameter

--- CleanSweepV2/src/CleanSweepModels/Home.java
+++ CleanSweepV2/src/CleanSweepModels/Home.java
@@ -1,5 +1,6 @@
 package CleanSweepModels;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -11,17 +12,51 @@ public class Home {
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
     	
-    	FloorPlan fp = (XMLParse.ParserFloorPlan.runParser("XMLParse/BigXML.xml"));
+    	String path = "src/XMLParse/BigXML.xml";
     	
-    	
-    	System.out.println(fp.getRobot().toString());
-    	System.out.println(fp.toString());
-    	
-        fp.MoveRobot(new ArrayList<Point>());
-        System.out.println(fp.toString());
-        System.out.println(fp.getRobot().toString());
-        fp.getRobot().dumpLog();
+    	if (args.length > 0) {
+			if (isValidXML(args[0]))
+				path = args[0];
+		}
+		File f = new File(path);
+		//startRobot(f.getCanonicalPath());
+
+		startRobot(path);
         
     }
+    public static void startRobot(String path) throws ParserConfigurationException, SAXException, IOException {
+		FloorPlan fp = (XMLParse.ParserFloorPlan
+				.runParser(path));
+		
+		System.out.println(fp.getRobot().toString());
+		System.out.println(fp.toString());
+
+		fp.MoveRobot(new ArrayList<Point>());
+		System.out.println(fp.getRobot().toString());
+		System.out.println("Floor is clean");
+		fp.getRobot().dumpLog();
+	}
+
+	// returns false if not a existing XML file
+	public static boolean isValidXML(String path) throws IOException {
+		File f = new File(path);
+		//String workingDir = System.getProperty("user.dir");
+		//File f2 = new File(workingDir + "/" + path);
+		       
+		if (f.exists() && !f.isDirectory()) {
+			// check if XML
+			String extension = "";
+
+			int i = path.lastIndexOf('.');
+			if (i > 0) {
+				extension = path.substring(i + 1);
+				if (extension.equalsIgnoreCase("XML"))
+					return true;
+			}
+			return false;
+		} else
+			return false;
+	}
+
 
 }
