403174d10ab3d41b76149d67440906f79d4df83b
unknown <biswas19@yahoo.com>
2014-11-07 19:21:26 -0600

    Modify test to new robot change

--- CleanSweepV2/src/test/cleansweep/RobotMoveTest.java
+++ CleanSweepV2/src/test/cleansweep/RobotMoveTest.java
@@ -13,6 +13,7 @@ import org.xml.sax.SAXException;
 
 import XMLParse.FloorCell;
 import CleanSweepModels.*;
+import CleanSweepModels.Types.FloorTypes;
 
 
 public class RobotMoveTest {
@@ -22,36 +23,51 @@ public class RobotMoveTest {
 	private Robot robot =null;
 
 	@Test
-	public void testMove01() throws ParserConfigurationException, SAXException, IOException  {
+	public void testMoveY() throws ParserConfigurationException, SAXException, IOException  {
 		robot =null;
 		initRobot(); 
-		robot.Move(new Point(0, 1));
-		assertEquals(robot.getCoordinates().getX(),0);
-		assertEquals(robot.getCoordinates().getY(),1);
+		FloorCell fc = new FloorCell() ;
+		fc.setXCoordinates(0);
+		fc.setYCoordinates(1);
+		robot.Move(fc,true);
+		assertEquals(0,robot.getCoordinates().getX());
+		assertEquals(1,robot.getCoordinates().getY());
+
 	}
 
 	@Test
-	public void testMove02() throws ParserConfigurationException, SAXException, IOException  {
+	public void testMoveX() throws ParserConfigurationException, SAXException, IOException  {
 		robot =null;
 		initRobot(); 
-		robot.Move(new Point(0, 1));
-		robot.Move(new Point(0, 2));
-		assertEquals(robot.getCoordinates().getX(),0);
-		assertEquals(robot.getCoordinates().getY(),2);
+		FloorCell fc = new FloorCell() ;
+		fc.setXCoordinates(1);
+		fc.setYCoordinates(0);
+		robot.Move(fc,true);
+		assertEquals(robot.getCoordinates().getX(),1);
+		assertEquals(robot.getCoordinates().getY(),0);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testMoveMoreThanCell() throws ParserConfigurationException, SAXException, IOException, IllegalArgumentException  {
+	public void testMoveTwoYCordinate() throws ParserConfigurationException, SAXException, IOException, IllegalArgumentException  {
 		robot =null;
 		initRobot(); 
-		robot.Move(new Point(1, 1));
+		FloorCell fc = new FloorCell() ;
+		fc.setXCoordinates(0);
+		fc.setYCoordinates(2);
+		robot.Move(fc,true);
+		
 		}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testMoveMinusCordinate() throws ParserConfigurationException, SAXException, IOException, IllegalArgumentException  {
+	public void testMoveTwoXCordinate() throws ParserConfigurationException, SAXException, IOException, IllegalArgumentException  {
 		robot =null;
 		initRobot(); 
-		robot.Move(new Point(-1, 1));
+		FloorCell fc = new FloorCell() ;
+		fc.setXCoordinates(2);
+		fc.setYCoordinates(0);
+		robot.Move(fc,true);
+		
+
 		}
 	
 	private void initRobot()throws ParserConfigurationException, SAXException,IOException 
