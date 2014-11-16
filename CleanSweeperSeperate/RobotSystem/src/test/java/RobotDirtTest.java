bdf621f493b16fdbfd12b697b8ebba924bc521e1
Chris Lacy <chris.c.lacy@gmail.com>
2014-11-16 08:48:33 -0600

    Build error fix

--- CleanSweeperSeperate/RobotSystem/src/test/java/RobotDirtTest.java
+++ CleanSweeperSeperate/RobotSystem/src/test/java/RobotDirtTest.java
@@ -9,7 +9,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
-import src.main.java.*;
 import src.main.java.CleanSweepModels.*;
 import src.main.java.XMLParse.FloorPlan;
 import src.main.java.XMLParse.ParserFloorPlan;
