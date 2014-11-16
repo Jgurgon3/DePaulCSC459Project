a1db4ed70082488950d6cee9a131f193e813584c
Matt Stillo <mstillo26@gmail.com>
2014-11-15 19:02:11 -0600

    Fixed issues with 1.7 compiler.

--- CleanSweeperSeperate/SensorSystem/src/main/java/XMLParse/FloorCell.java
+++ CleanSweeperSeperate/SensorSystem/src/main/java/XMLParse/FloorCell.java
@@ -2,8 +2,8 @@ package src.main.java.XMLParse;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+//import java.util.function.Predicate;
+//import java.util.stream.Collectors;
 
 //import src.main.java.CleanSweepModels.*;
 
@@ -218,29 +218,31 @@ public class FloorCell implements Comparable<FloorCell> {
 	  }	
 	 
 	  
-	  public static Predicate<FloorCell> FindByCordinate(Point searchPoint, List<FloorCell> floorList) {
+//	  public static Predicate<FloorCell> FindByCordinate(Point searchPoint, List<FloorCell> floorList) {
 //		  final List<FloorCell> selection = new ArrayList<FloorCell>();
-		  Predicate<FloorCell> cell2 = null;
-		  for(FloorCell cell : floorList){
-		  if(cell.getCoordinates().getX()==searchPoint.getX() &&   cell.getCoordinates().getY()==searchPoint.getY())
-			  cell2 = (Predicate<FloorCell>) cell; 
-		  } 
-		 
-		return cell2;
-	  }
+//		  Predicate<FloorCell> cell2 = null;
+//		  for(FloorCell cell : floorList){
+//		  if(cell.getCoordinates().getX()==searchPoint.getX() &&   cell.getCoordinates().getY()==searchPoint.getY())
+//			  cell2 = (Predicate<FloorCell>) cell; 
+//		  } 
+//		 
+//		return cell2;
+//	  }
+//	  
+//	  public static List<FloorCell> filterFloorCell (List<FloorCell> floorcells, Predicate<FloorCell> predicate) {
+//		  List<FloorCell> lst =floorcells.stream().filter( predicate ).collect(Collectors.<FloorCell>toList()); 
+//	        return lst;
+//	    }
 	  
-	  public static List<FloorCell> filterFloorCell (List<FloorCell> floorcells, Predicate<FloorCell> predicate) {
-		  List<FloorCell> lst =floorcells.stream().filter( predicate ).collect(Collectors.<FloorCell>toList()); 
-	        return lst;
-	    }
-	  
-	  public static FloorCell FindFloorCell (List<FloorCell> floorList,Point p ) {
-			List<FloorCell> floorCellFlistresult = (List<FloorCell>) FloorCell.filterFloorCell(floorList,FloorCell.FindByCordinate(p, floorList) );
-			if(floorCellFlistresult!=null && floorCellFlistresult.size()>0)
-			{
-				return floorCellFlistresult.get(0);
-				}
-			else
-				return null;
-	    }
+//	  public static FloorCell FindFloorCell (List<FloorCell> floorList,Point p ) 
+//	  {
+//			List<FloorCell> floorCellFlistresult = (List<FloorCell>) FloorCell.filterFloorCell(floorList,FloorCell.FindByCordinate(p, floorList) );
+//			if(floorCellFlistresult!=null && floorCellFlistresult.size()>0)
+//			{
+//				return floorCellFlistresult.get(0);
+//				}
+//			else
+//				return null;
+		  
+//	    }
 }
